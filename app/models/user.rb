b9bef9e7fe6040bc9608c4d6f291e57f8649dc20
Sasha Chapin <sasha.chapin@gmail.com>
2011-01-11 17:07:14 -0500

    Added secure one-way hash for passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
   
+  before_save :encrypt_password
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence => true,
@@ -23,5 +25,28 @@ class User < ActiveRecord::Base
                 :uniqueness => { :case_sensitive => false }
   validates :password, :presence     => true,
                        :confirmation => true,
-                       :length       => { :within => 6..40 }
+                       :length       => { :within => 6..40}
+                       
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+                    
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -85,5 +85,31 @@ describe User do
       User.new(hash).should_not be_valid
     end
   end
+  
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    describe "has_password? method" do
+
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end    
+
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+  end
     
 end
