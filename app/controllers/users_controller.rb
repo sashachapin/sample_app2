88d65a1cba87d9b901bea29935901c07a15fa789
Sasha Chapin <sasha.chapin@gmail.com>
2011-01-12 13:21:08 -0500

    Can create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,19 @@ class UsersController < ApplicationController
 
   def new
     @title = "Sign up"
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = ""
+      @user.password_confirmation = ""
+      render 'new'
+    end
   end
 end
