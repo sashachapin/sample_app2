ee66d5a6309c6982e7afa9c20c2efc5aaf5f3b67
Sasha Chapin <sasha.chapin@gmail.com>
2011-01-13 11:33:39 -0500

    Have user signin / signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users 
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
   match '/signup', :to => 'users#new'
 
